ddd69afdf77e03368b2e37e6610d3080dfc2f007
SornDevt <sorndev.teach@gmail.com>
2025-05-15 22:36:11 +0700

    install template html5 to project VDO 02

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,15 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My App MiniPos</title>
+
+     <link rel="stylesheet" href="{{ url('assets/vendor/css/core.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/css/demo.css') }}">
+
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
     @vite('resources/css/app.css')
 </head>
 <body>
 
-<h1> Hello Laravel ooooooooooo</h1>
-    <hr>
     <div id="app"></div>
 
 
     @vite('resources/js/app.js')
+
+
+       <script src="{{ url('assets/vendor/libs/jquery/jquery.js') }}"></script>
+    
+    <script src="{{ url('assets/vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ url('assets/vendor/js/bootstrap.js') }}"></script>
+
+    
+
+    
+      <script src="{{ url('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+      
+      <script src="{{ url('assets/vendor/js/menu.js') }}"></script>
+    
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="{{ url('assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
+
+    <!-- Main JS -->
+    
+      <script src="{{ url('assets/js/main.js') }}"></script>
+
+
 </body>
 </html>
